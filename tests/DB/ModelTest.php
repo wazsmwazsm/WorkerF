a1973960de23513a5e0d567973aa4b0733e2e160
wazsmwazsm <18748406022@163.com>
2017-12-28 19:43:06 +0800

    add ModelTest

---
+++ tests/DB/ModelTest.php
@@ -0,0 +1,52 @@
+<?php
+use WorkerF\DB\DB;
+use WorkerF\DB\Model;
+
+class TModel extends Model {
+
+    protected $connection = 'con1';
+
+    protected $table = 'user';
+}
+
+class ModelTest extends PHPUnit_Framework_TestCase
+{
+    protected static $model;
+
+    public static function setUpBeforeClass()
+    {
+        $conf = [
+            'con1' => [
+                'driver'   => 'mysql',
+                'host'     => 'localhost',
+                'port'     => '3306',
+                'user'     => 'root',
+                'password' => '',
+                'dbname'   => 'test',
+                'prefix'   => 't_',
+                'charset'  => 'utf8',
+            ],
+        ];
+
+        DB::init($conf);
+
+        self::$model = new TModel();
+    }
+
+    public static function tearDownAfterClass()
+    {
+        self::$model = NULL;
+    }
+
+    public function testModel()
+    {
+        $foo = self::$model->select('*');
+        $this->assertTrue($foo instanceof \WorkerF\DB\Drivers\Mysql);
+    }
+
+    public function testModelCallStatic()
+    {
+        $foo = TModel::select('*');
+        $this->assertTrue($foo instanceof \WorkerF\DB\Drivers\Mysql);
+    }
+}

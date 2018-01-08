960e1b99b53ee69d909c46f4768422ddbb94dc0c
wazsmwazsm <18748406022@163.com>
2018-01-08 19:47:24 +0800

    update route test

--- tests/Http/RouteTest.php
+++ tests/Http/RouteTest.php
@@ -3,12 +3,12 @@ use WorkerF\Http\Route;
 
 class RouteFake extends Route
 {
-    public static function getMapTree($uri)
+    public static function getMapTree()
     {
         return self::$_map_tree;
     }
 
-    public static function getFilter($uri)
+    public static function getFilter()
     {
         return self::$_filter;
     }
@@ -43,5 +43,40 @@ class RouteTest extends PHPUnit_Framework_TestCase
         $result = RouteFake::namespaceParse('\\a\\\\a\\\\\\g\d');
 
         $this->assertEquals("\\a\\a\\g\\d", $result);
+    }
+
+    public function testSetRoute()
+    {
+        // GET, callback
+        RouteFake::get('/a', function() {
+            return 'a';
+        });
+
+        $map = RouteFake::getMapTree();
+
+        $this->assertEquals('a', $map['/a']['GET']());
+
+        // GET, string
+        RouteFake::get('/b', 'Test\Controller@get');
+
+        $map = RouteFake::getMapTree();
+
+        $this->assertEquals('Test\Controller@get', $map['/b']['GET']);
+
+        // POST
+        RouteFake::post('/c', function() {
+            return 'c';
+        });
+
+        $map = RouteFake::getMapTree();
+
+        $this->assertEquals('c', $map['/c']['POST']());
+
+        // PUT
+        RouteFake::put('/d', 'Test\Controller@get');
+
+        $map = RouteFake::getMapTree();
+
+        $this->assertEquals('Test\Controller@get', $map['/d']['PUT']);
     }
 }
